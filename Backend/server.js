5cfd22434a3231e9d8497778f687c065f3b7d9ab
Sandeepsahu18 <editingking998@gmail.com>
2026-09-13 12:51:44 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import morgan from "morgan";
+const PORT = 3000
+const app = express();
+app.use(morgan("dev"));
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "OK" });
+});
+
+app.get("/api/hello", (req, res) => {
+  res.status(200).json({ status: "Hello World !!" });
+});
+
+app.get("/api/users", (req, res) => {
+    const users = [
+        {id:1, name: "sandeep"},
+        {id:2, name: "sagar"},
+        {id:3, name: "deep"},
+    ]
+
+    res.status(200).json(users)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
