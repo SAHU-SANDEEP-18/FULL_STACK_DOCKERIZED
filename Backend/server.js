182e4271162b7c8f81362a6a40414cd7dc963040
Sandeepsahu18 <editingking998@gmail.com>
2026-09-14 22:46:01 +0530

    final

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-const PORT = 3000
+const PORT = 3000;
 const app = express();
 app.use(morgan("dev"));
 
@@ -13,15 +13,16 @@ app.get("/api/hello", (req, res) => {
 });
 
 app.get("/api/users", (req, res) => {
-    const users = [
-        {id:1, name: "sandeep"},
-        {id:2, name: "sagar"},
-        {id:3, name: "deep"},
-    ]
+  const users = [
+    { id: 1, name: "sandeep" },
+    { id: 2, name: "sagar" },
+    { id: 3, name: "deep" },
+    { id: 4, name: "deepka" },
+  ];
 
-    res.status(200).json(users)
-})
+  res.status(200).json(users);
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,14 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    proxy: {
+      "/api" : {
+        target: "http://Backend:3000/",
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
